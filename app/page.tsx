0f6522dab55b1dfbbb47073f5b547fd27474f1d6
Wajeeh <alamwajeeh@gmail.com>
2025-01-27 23:54:16 -0500

    base started, must finish most tmr

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
+import Hero from '@/components/Hero';
+import { FloatingNav } from '@/components/ui/floating-navbar';
+import { FaHome } from 'react-icons/fa';
+import Grid from '@/components/Grid';
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hi</h1>
+    <main className="relative bg-blue1 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {name: "Home", link: "/", icon: <FaHome />},
+        ]}/>
+        <Hero />
+        <Grid />
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,15 @@
 import type { Config } from "tailwindcss";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+import svgToDataUri from "mini-svg-data-uri";
+
+const addVariablesForColors = ({ addBase, theme }: any) => {
+  const colors = flattenColorPalette(theme("colors"));
+  const cssVars = Object.fromEntries(
+    Object.entries(colors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({ ":root": cssVars });
+};
 
 export default {
   content: [
@@ -8,11 +19,96 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        first: "moveVertical 30s ease infinite",
+        second: "moveInCircle 20s reverse infinite",
+        third: "moveInCircle 40s linear infinite",
+        fourth: "moveHorizontal 40s ease infinite",
+        fifth: "moveInCircle 20s ease infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        moveHorizontal: {
+          "0%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+          "50%": {
+            transform: "translateX(50%) translateY(10%)",
+          },
+          "100%": {
+            transform: "translateX(-50%) translateY(-10%)",
+          },
+        },
+        moveInCircle: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "50%": {
+            transform: "rotate(180deg)",
+          },
+          "100%": {
+            transform: "rotate(360deg)",
+          },
+        },
+        moveVertical: {
+          "0%": {
+            transform: "translateY(-50%)",
+          },
+          "50%": {
+            transform: "translateY(50%)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%, -40%) scale(1)",
+          },
+        },
+        shimmer: {
+          from: { backgroundPosition: "0 0" },
+          to: { backgroundPosition: "-200% 0" },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+        },
+        blue1: {
+          DEFAULT: "#0F0F1B",
+          dark: "#000319",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+    ({ matchUtilities, theme }: any) => {
+      matchUtilities(
+        {
+          "bg-grid": (value: string) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: string) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" cx="10" cy="10" r="1.6"/></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 } satisfies Config;
